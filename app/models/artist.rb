57445ee4e5038c8ca7d8392b98239ed20313c38f
Beth Cummings <bethios@gmail.com>
2017-06-10 15:39:55 -0500

    fixing deploy

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,24 +4,24 @@ class Artist < ActiveRecord::Base
 
   has_attached_file :main_image,
                     styles: {
-                        small: '300x300',
-                        large: '500x500'
+                        small: '300x300#',
+                        large: '500x500#'
                     }
 
   validates_attachment_content_type :main_image, :content_type => /\Aimage\/.*\z/
 
   has_attached_file :image_2,
                     styles: {
-                        small: '300x300',
-                        large: '500x500'
+                        small: '300x300#',
+                        large: '500x500#'
                     }
 
   validates_attachment_content_type :image_2, :content_type => /\Aimage\/.*\z/
 
   has_attached_file :image_3,
                     styles: {
-                        small: '300x300',
-                        large: '500x500'
+                        small: '300x300#',
+                        large: '500x500#'
                     }
 
   validates_attachment_content_type :image_3, :content_type => /\Aimage\/.*\z/
